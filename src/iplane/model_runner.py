Corrected calculation of accuracy.

src/iplane/model_runner.py
@@ -123,10 +123,7 @@ class ModelRunner(object):
         prediction_tnsr = self.predict(feature_tnsr)
         prediction_df = pd.DataFrame(prediction_tnsr.to(cn.CPU).numpy(), columns=columns)
         target_df = pd.DataFrame(target_tnsr.to(cn.CPU).numpy(), columns=columns)
-        numerator_arr = np.abs(prediction_df - target_df)/target_df
-        arr1 = numerator_arr/target_df.values
-        arr2 = numerator_arr/prediction_df.values
-        plot_df = pd.DataFrame(np.maximum(arr1, arr2), columns=columns)
+        plot_df = (prediction_df - target_df).abs()/target_df
         plot_df['class'] = "relative error"
         # Plot
         if ax is None:

src/iplane/model_runner_nn.py
@@ -97,9 +97,7 @@ class ModelRunnerNN(ModelRunner):
             accuracy (float)
         """
         prediction_tnsr = self.predict(feature_tnsr)
-        accuracy1_tnsr = torch.abs(prediction_tnsr - target_tnsr)/target_tnsr <= self.max_fractional_error
-        accuracy2_tnsr = torch.abs(prediction_tnsr - target_tnsr)/prediction_tnsr <= self.max_fractional_error
-        accuracy_tnsr = accuracy1_tnsr & accuracy2_tnsr
+        accuracy_tnsr = torch.abs(prediction_tnsr - target_tnsr)/target_tnsr <= self.max_fractional_error
         accurate_rows = torch.sum(accuracy_tnsr, dim=1) == accuracy_tnsr.shape[1]
         accuracy = (torch.sum(accurate_rows) / accurate_rows.shape[0]).item()
         return accuracy
@@ -118,9 +116,7 @@ class ModelRunnerNN(ModelRunner):
         prediction_arr = prediction_tnsr.cpu().numpy()
         target_arr = target_tnsr.cpu().numpy()
         # Find deiviations handling small and large predictions
-        mae1_arr = np.max(np.abs(prediction_arr - target_arr) / target_arr, axis=1)
-        mae2_arr = np.max(np.abs(prediction_arr - target_arr) / prediction_arr, axis=1)
-        mae_arr = np.maximum(mae1_arr, mae2_arr)
+        mae_arr = np.max(np.abs(prediction_arr - target_arr) / target_arr, axis=1)
         # Smooth the inaccuracy
         smoothed_inaccuracy = np.mean(mae_arr)
         return smoothed_inaccuracy